679cb76cfdd210f00f051e2ba20d6fbfb5e8da08
osakirii <heitorcestari66@gmail.com>
2024-10-08 23:13:58 -0300

    ajudado

--- bookersgalaxy/modulos/callimg.php
+++ bookersgalaxy/modulos/callimg.php
@@ -1,6 +1,6 @@
 <?php 
     function Src($id){ 
-        include_once("../connect.php");
+        include_once("connect.php");
 
         $results = $mysqli->query("SELECT * from arquivos where id = $id") or die($mysqli->error);
         $mostrar = $results->fetch_assoc();
