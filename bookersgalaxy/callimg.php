346741da96157ef7cee1c5e459289d7a84e9f75d
Felipe Vieira <129530532+lipedeoliveira@users.noreply.github.com>
2024-11-08 11:14:25 -0300

    Update com Remoção
    Update com remoção de funções duplicadas em ambos arquivos

--- bookersgalaxy/callimg.php
+++ bookersgalaxy/callimg.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function Busca($id) {
     global $pdo; 
@@ -16,21 +15,3 @@ function Busca($id) {
             'id_livro'=>$mostrar['id_livro']
         ];    }
 }
-?>
-=======
-<?php
-function Busca($id) {
-    global $pdo; 
-    
-    $stmt = $pdo->prepare("SELECT path FROM arquivos WHERE id = :id"); // Confirme se 'path' é o nome correto do campo
-    $stmt->bindParam(":id", $id, PDO::PARAM_INT);
-    $stmt->execute();
-
-    if ($mostrar = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        return $mostrar['path'];
-    } else {
-        return '../img/icons/blocked_icon.png'; // Caminho para imagem padrão
-    }
-}
-?>
->>>>>>> 70a5a7598c8c2a87cfa34de9530ee513efdcc22e
