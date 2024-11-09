ec3b50599a4174d4331adde44140cccb8610f720
osakirii <heitorcestari66@gmail.com>
2024-11-08 22:33:39 -0300

    dando commit só pra pegar as mudanças q o felipe fez

--- bookersgalaxy/perfil.php
+++ bookersgalaxy/perfil.php
@@ -1,3 +1,17 @@
 <?php
-    include_once("../modulos/header.php");
+    include_once("modulos/header.php");
 ?>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/perfil.css">
+        <title>Perfil</title>
+    </head>
+    <body>
+        <main id="corpo">
+            <img src="img/usuario/placeholder.png" alt="placeholder.png">
+        </main>
+    </body>
+</html>
