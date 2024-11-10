091025ed0edbd6b41b13daa94ca19360560d5642
Felipe Vieira <129530532+lipedeoliveira@users.noreply.github.com>
2024-11-09 21:01:55 -0300

    Update com adição de função
    Adicionada a função para buscar somente a capa do livro, juntamente com titulo,autor,preco e id ( as vozes estão ganhando força )

--- bookersgalaxy/modulos/callimg.php
+++ bookersgalaxy/modulos/callimg.php
@@ -1,15 +1,44 @@
-<?php
-function Busca($id) {
-    global $pdo; 
-    
-    $stmt = $pdo->prepare("SELECT path FROM arquivos WHERE id = :id"); // Confirme se 'path' é o nome correto do campo
-    $stmt->bindParam(":id", $id, PDO::PARAM_INT);
-    $stmt->execute();
+    <?php
+include_once(__DIR__ . '/../config.php'); // Inclui todas as configurações e funções globais
 
-    if ($mostrar = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        return $mostrar['path'];
-    } else {
-        return '../img/icons/blocked_icon.png'; // Caminho para imagem padrão
+    $con = Connect::getInstance();
+    function Busca($id) {
+
+        global $pdo;
+        
+        $stmt = $pdo->prepare("SELECT path FROM arquivossite WHERE id = :id"); // Confirme se 'path' é o nome correto do campo
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        if ($mostrar = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            return "/bookersgalaxy/" . $mostrar['path']; // Adiciona o caminho base manualmente
+        } else {
+            echo 'Deu erro no callimg';
+        }
     }
-}
-?>
+    function BuscaLivro(){
+        global $pdo;
+        
+        // Prepare a query para buscar os dados
+        $stmtImagem = $pdo->prepare("
+            SELECT 
+                arquivos.path, 
+                livros.Titulo, 
+                livros.Autor, 
+                livros.Preco, 
+                livros.id_livro
+            FROM arquivos 
+            INNER JOIN livros 
+                ON livros.id_livro = arquivos.livro_id 
+            WHERE arquivos.is_capa = 1
+        ");
+        
+        // Executa a consulta
+        $stmtImagem->execute();
+        
+        // Obtém os resultados da consulta
+        return $stmtImagem->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+    
+    ?>
