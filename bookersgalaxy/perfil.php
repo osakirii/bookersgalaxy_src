742d9e5223b009a8f065e14a2e06926d80798058
osakirii <heitorcestari66@gmail.com>
2024-11-10 13:47:29 -0300

    começo do perfil.php

--- bookersgalaxy/perfil.php
+++ bookersgalaxy/perfil.php
@@ -22,16 +22,34 @@
 
             <div id="perfil">
                 <img src="img/usuario/placeholder.png" alt="placeholder.png">
-                <div id="perfilContainer">
+                <div class="perfilContainer">
                     <p>| JORGE VALENTIM</p>
                     <p>| Biografia: x x x x x xxxxx xxxxxxxxx xxx xxx xxxxx xx xxx xxxx x xx
                          xxxxxx xx xx xxx xx xx xxxxxx x x x x x </p>
                     <span>
                         <i class="fas fa-pen-to-square"></i><p>Editar Perfil</p>
+                        </span>
+                    <span>
                         <i class="fas fa-address-book"></i><p>Dados Pessoais</p>
-                    </span>
+                        </span>
                 </div>
-                <div class="perfilContainer"></div>
+                <div class="perfilContainer">
+                    <span>
+                        <i class="fas fa-cart-shopping"></i><p>Carrinho</p>
+                        </span>
+                    <span>
+                        <i class="fas fa-location-dot"></i><p>Endereços Cadastrados</p>
+                        </span>
+                    <span>
+                        <i class="fas fa-credit-card"></i><p>Cartões Cadastrados</p>
+                        </span>
+                    <span>
+                        <i class="fas fa-boxes-packing"></i><p>Pedidos</p>
+                        </span>
+                    <span>
+                        <i class="far fa-heart"></i><p>Favoritos</p>
+                        </span>
+                </div>
             </div>
         </main>
     </body>
