35eb3f6c4d2dc1a0bc1c739b8cbce1bb4810c0c1
osakirii <heitorcestari66@gmail.com>
2024-11-11 16:28:29 -0300

    upload de pedidos.php (css tá no perfil.css)

--- perfil/pedidos.php
+++ perfil/pedidos.php
@@ -12,9 +12,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/perfil.css">
     <title>Pedidos</title>
 </head>
 <body>
-    
+    <main id="corpo">
+        <h1>PEDIDOS</h1>
+        <section class="pedido">
+            <h2>Pedido: [NOME DO PRODUTO]</h2>
+            <div class="pedidoContainer">
+                <img src="../img/usuario/placeholder.png" alt="placeholder.png">
+                <div class="timeline">
+                    <span class="timelineContainer active">
+                        <p>Pedido feito. (HORAS)</p>
+                        <p class="desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia nesciunt minus soluta qui ducimus error quidem saepe, natus, corrupti dolor sapiente, labore ipsam provident dolorum dicta molestiae aut est repudiandae.</p>
+                    </span>
+                    <span class="timelineContainer">
+                        <p>Pedido feito. (HORAS)</p>
+                        <p class="desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia nesciunt minus soluta qui ducimus error quidem saepe, natus, corrupti dolor sapiente, labore ipsam provident dolorum dicta molestiae aut est repudiandae.</p>
+                    </span>
+                    <span class="timelineContainer">
+                        <p>Pedido feito. (HORAS)</p>
+                        <p class="desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia nesciunt minus soluta qui ducimus error quidem saepe, natus, corrupti dolor sapiente, labore ipsam provident dolorum dicta molestiae aut est repudiandae.</p>
+                    </span>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
