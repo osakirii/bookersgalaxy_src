6d2d10ba1c5e12a3c6f37decec565626da05228e
osakirii <heitorcestari66@gmail.com>
2024-11-15 15:16:30 -0300

    carrinho.css arrumado talvez nao sei

--- index.php
+++ index.php
@@ -27,6 +27,26 @@ if (isset($_SESSION['cliente_id'])) {
 
 <body>
     <main id="corpo">
+                <!--CARROSSEL DE BANNERS-->
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?php echo Busca(7) ?>" alt="Primeiro Slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php echo Busca(7) ?>" alt="Segundo Slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Próximo</span>
+            </a>
+        </div>
+
         <h1>TOP 10 DO MÊS</h1>
         <section class="carrossel">
             <?php
