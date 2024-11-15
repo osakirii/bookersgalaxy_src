86fc45b78c8874b50762b6836a81ac8f84fe3e03
osakirii <heitorcestari66@gmail.com>
2024-11-15 17:01:46 -0300

    arrumando mais ainda o carrinho e a paginas de login

--- perfil/cadastro.php
+++ perfil/cadastro.php
@@ -19,11 +19,11 @@
                 <p>Nome Completo<br><input required id="nomeCompleto" name="nomeCompleto" type="text" size="20"></p>
                 <p>Nome de Usuário<br><input required id="nomeUsuario" name="nomeUsuario" type="text" size="20"></p>
                 <p>Email<br><input required id="email" name="email" type="text" size="20"></p>
-                <p>Telefone<br><input required id="telefone" name="telefone" type="text" size="20" oninput="mascaraTelefone(this)" maxlength="15"></p>
-                <p>CPF<br><input required id="cpf" name="cpf" type="text" size="20" oninput="mascaraCPF(this)" maxlength="14"></p>
                 <p>Senha<br><input required id="senha" name="senha" type="password" size="20"></p>
+                <p>Telefone<br><input required id="telefone" name="telefone" type="text" size="20" oninput="mascaraTelefone(this)" maxlength="15"></p>
                 <p>Confirme sua Senha<br><input required id="confSenha" name="confSenha" type="password" size="20"></p>
-                <p>Li e acordo com os Termos <input type="checkbox" name="checkbox" required id="checkbox"></p>
+                <p>CPF<br><input required id="cpf" name="cpf" type="text" size="20" oninput="mascaraCPF(this)" maxlength="14"></p>
+                <p id="checkbox">Li e acordo com os Termos <input type="checkbox" name="checkbox" required id="checkbox"></p>
             </div>
             <div class="btns">
                 <button class="BTazul" onclick="window.history.back()">Voltar</button>
